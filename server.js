d19487397dce29753dc7b22837dafb9146b0d7fa
Elizabeth Munoz <munoz_714@live.com>
2020-09-18 19:00:44 -0700

    mongoose setup

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -6,6 +7,8 @@ const PORT = process.env.PORT || 3001;
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooksearch");
 
 app.listen(PORT, function () {
     console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
